270bb9359800d32b97862fb0cbfc570ceae54085
miladr <miladr@gmail.com>
2014-06-06 14:37:28 +0430

    Create JDateTime.php

---
+++ Facades/JDateTime.php
@@ -0,0 +1,14 @@
+<?php namespace milad\jalali\Facades;
+ 
+use Illuminate\Support\Facades\Facade;
+ 
+class JDateTime extends Facade {
+ 
+  /**
+   * Get the registered name of the component.
+   *
+   * @return string
+   */
+  protected static function getFacadeAccessor() { return 'JDateTime'; }
+ 
+}
