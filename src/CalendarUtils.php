d5a7e99538ee13ddb8f0a735d2ab2fe02d5381e0
hasanmonfared <hasandoulabi@gmail.com>
2022-04-05 18:08:50 +0430

    Fix warning Implicit conversion from float to int loses in PHP 8.1

--- src/CalendarUtils.php
+++ src/CalendarUtils.php
@@ -202,11 +202,10 @@ class CalendarUtils
     /**
      * @param $a
      * @param $b
-     * @return float
      */
-    public static function div($a, $b)
+    public static function div($a, $b):int
     {
-        return ~~(intval($a / $b));
+        return  intdiv($a ,$b);
     }
 
     /**
@@ -214,9 +213,9 @@ class CalendarUtils
      * @param $b
      * @return mixed
      */
-    public static function mod($a, $b)
+    public static function mod($a, $b):int
     {
-        return $a - ~~(intval($a / $b)) * $b;
+        return  $a - intdiv($a , $b) * $b;
     }
 
     /**
